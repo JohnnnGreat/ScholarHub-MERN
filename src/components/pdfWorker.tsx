b301b1112f36f4c75ab07c23f16c754a9537022e
John Ossai <johnossai20@gmail.com>
2024-06-22 00:22:35 +0100

    ll

--- src/components/pdfWorker.tsx
+++ src/components/pdfWorker.tsx
@@ -1,12 +1,11 @@
 "use client";
 import React from "react";
 import { pdfjs } from "react-pdf";
-
+pdfjs.GlobalWorkerOptions.workerSrc = new URL(
+  "pdfjs-dist/build/pdf.worker.min.mjs",
+  import.meta.url
+).toString();
 const PDFWorker = ({ children }: { children: React.ReactNode }) => {
-  pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-    "pdfjs-dist/build/pdf.worker.min.mjs",
-    import.meta.url
-  ).toString();
   return <div>{children}</div>;
 };
 
